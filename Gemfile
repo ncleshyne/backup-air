6a43da7e47f09f6d4c6bc29ab4deabe6cfc284f9
ncleshyne <nicoleshayne.elem@gmail.com>
2017-08-08 15:41:59 +0800

    Initial commit

--- Gemfile
+++ Gemfile
@@ -35,8 +35,6 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
-gem 'omniauth'
-gem 'omniauth-facebook'
 gem 'geocoder'
 gem 'simple_form'
 gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :registrations => "registrations", :omniauth_callbacks => "users/omniauth_callbacks" }
   get 'profiles/show'
-  get ':user_name', to: 'profiles#show', as: :profile
-  get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
-  patch ':user_name/edit', to: 'profiles#update', as: :update_profile
-  root 'posts#index'
+  devise_for :users, :controllers => { :registrations => "registrations" }
   resources :posts do
     resources :comments
   end
+  root 'posts#index'
+  get ':user_name', to: 'profiles#show', as: :profile
+  get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
+  patch ':user_name/edit', to: 'profiles#update', as: :update_profile
 end
